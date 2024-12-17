ab274756fd898d16de8b40610136740721452b79
Kagaries <unconfigured@null.spigotmc.org>
2024-12-17 12:58:57 -0600

    Added translatables for all the things

--- src/main/java/com/kagaries/fabric/world/item/ModItems.java
+++ src/main/java/com/kagaries/fabric/world/item/ModItems.java
@@ -8,6 +8,7 @@ import io.wispforest.owo.itemgroup.OwoItemGroup;
 import io.wispforest.owo.itemgroup.OwoItemSettings;
 import io.wispforest.owo.registration.annotations.RegistryNamespace;
 import io.wispforest.owo.registration.reflect.ItemRegistryContainer;
+import net.fabricmc.fabric.api.item.v1.CustomDamageHandler;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.AxeItem;
@@ -26,15 +27,20 @@ public class ModItems implements ItemRegistryContainer {
 
     public static final OwoItemGroup ITEM_GROUP = OwoItemGroup.builder(new Identifier(Magia.MOD_ID, "me-fabric"), () -> Icon.of(ModItems.EMERALD_AXE))
             .initializer(owoItemGroup -> {
+                owoItemGroup.addCustomTab(Icon.of(ModMobItems.BLUE_SLIME_BALL), "magia-edition", ((displayContext, entries) -> {
+                    entries.add(ModItems.EMERALD_AXE);
+                    entries.add(ModBlocks.TEST);
+                    entries.add(ModMobItems.BLUE_SLIME_BALL);
+                }), true);
                 owoItemGroup.addCustomTab(Icon.of(ModItems.EMERALD_AXE), "magia-weapons-and-tools", (displayContext, entries) -> {
                     entries.add(ModItems.EMERALD_AXE);
-                }, true);
+                }, false);
                 owoItemGroup.addCustomTab(Icon.of(ModBlocks.TEST), "magia-blocks", (displayContext, entries) -> {
                     entries.add(ModBlocks.TEST);
                 }, false);
                 owoItemGroup.addCustomTab(Icon.of(ModMobItems.BLUE_SLIME_BALL), "magia-mob-ingredients", (displayContext, entries) -> {
                     entries.add(ModMobItems.BLUE_SLIME_BALL);
-                }, true);
+                }, false);
             })
             .build();
 
